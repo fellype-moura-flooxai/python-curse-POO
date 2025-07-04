0dc8b562f8ca67b8f37d49e8e441354fbf55d1b9
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-07-04 07:01:54 -0300

    feat: settings of the dataclass decorator

--- aula38.py
+++ aula38.py
@@ -8,18 +8,16 @@
 from dataclasses import dataclass
 
 
-@dataclass(init=False)
+@dataclass(repr=True)
 class Pessoa:
     nome: str
     sobrenome: str
 
-    def __init__(self, nome, sobrenome):
-        self.nome = nome
-        self.sobrenome = sobrenome
-        self.nome_completo = f'{self.nome} {self.sobrenome}'
 
-    def __post_init__(self):
-        print('POST INIT')
+if __name__ == '__main__':
+    lista = [Pessoa('a', 'z'),Pessoa('b', 'y'),Pessoa('c', 'x'),]
+    ordenadas = sorted(lista, reverse=True, key=lambda p: p.sobrenome)
+    print(ordenadas)
 
     # @property
     # def nome_completo(self):
@@ -32,7 +30,7 @@ class Pessoa:
     #     self.sobrenome = ' '.join(sobrenome)
 
 
-if __name__ == '__main__':
-    p1 = Pessoa('Luiz', 'otavio')
-    print(p1)
-    print(p1.nome_completo)
+# if __name__ == '__main__':
+#     p1 = Pessoa('Luiz', 'otavio')
+#     print(p1)
+#     print(p1.nome_completo)
